9f2e8b53b3a9aeed9b47a0bee440b1064a3ea82c
unknown <s6330613008@phuket.psu.ac.th>
2024-05-09 13:11:38 +0700

    upload typescript

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,12 +1,14 @@
-
 import { Controller, Get, Post, UseInterceptors, Res, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { Response } from 'express';
 import * as path from "path";
+import { Request } from 'express';
+import { Req } from '@nestjs/common';
+
 
 interface FileParams {
-  fileName : string;
+  img : string;
 }
 
 
@@ -14,23 +16,30 @@ interface FileParams {
 export class FileController {
   
   @Post("upload")
-  @UseInterceptors(FileInterceptor('file',{
+  @UseInterceptors(FileInterceptor('singleFile',{
     storage: diskStorage({
       destination: "./assets",
       filename: (req, file, cb) => {
         const extArray = file.mimetype.split("/");
         const extension = extArray[extArray.length-1];
         const newFilename = "Fileupload-"+Date.now()+"."+extension;
+        req['uploadedFileName'] = newFilename; // เก็บชื่อไฟล์ที่อัปโหลดไว้ใน request object
         cb(null, newFilename)
       }
     })
   }))
-  async uploadFile() {
-    return 'success';
+  
+  async uploadFile(@Req() req: Request) {
+    const uploadedFileName = req['uploadedFileName'];
+    console.log(uploadedFileName);
+    return {"image": uploadedFileName }; // return ชื่อไฟล์ที่ถูกอัปโหลดในรูปแบบ JSON
+  
   }
 
-  @Get(':fileName')
+ 
+
+  @Get(':img')
   getFile(@Res() res: Response , @Param() file: FileParams) {
-    res.sendFile(path.join(__dirname, "../../assets/" + file.fileName));
+    res.sendFile(path.join(__dirname, "../../assets/" + file.img));
   }
 }
